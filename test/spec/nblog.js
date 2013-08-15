111eb1aa24d16d03f75472f86ac0e0fa3fc4034c
Alasdair Stalker <alasdair@asta.org.uk>
2013-08-15 23:32:27 +0100

    Added test for main nblog.js and updated errors

---
+++ test/spec/nblog.js
@@ -0,0 +1,22 @@
+var should = require('should'),
+    nblog = require('../../lib/nblog');
+
+describe('nblog', function() {
+  describe('#init()', function() {
+    var params = {
+      dev: {
+        user: 'user',
+        pass: 'password',
+        port: '3001',
+        db:   'mongodb://localhost/items'
+      }
+    };
+    params.prod = params.dev;
+
+    it('should initialise the app', function(done) {
+      var err = nblog.init(params);
+      done();
+    });
+  });
+
+});
