163193a15051d6840e624b58d36e283617741779
Alasdair Stalker <alasdair@asta.org.uk>
2013-08-29 21:57:47 +0100

    Switched Grunt watch to use Jasmine test and removed the grunt dependencies. They can be manually installed if required.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-jasmine-node');
   grunt.initConfig({
     watch: {
       scripts: {
@@ -11,15 +11,20 @@ module.exports = function (grunt) {
         }
       }
     },
-    mochacli: {
-        options: {
-            require: ['should'],
-            reporter: 'list'
-        },
-        all: ['test/**/*.js']
+    jasmine_node: {
+      specNameMatcher: "spec",
+      projectRoot: "test",
+      requirejs: false,
+      forceExit: true,
+      jUnit: {
+        report: false,
+        savePath : "./build/reports/jasmine/",
+        useDotNotation: true,
+        consolidate: true
+      }
     }
   });
-  grunt.registerTask('test', 'mochacli');
+  grunt.registerTask('test', 'jasmine_node');
   grunt.event.on('watch', function(action, filepath, target) {
     grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
   });
